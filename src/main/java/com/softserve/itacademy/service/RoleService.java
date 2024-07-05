6064e200a2d5876d5dac70da2aa6cd506946192e
SonnyRose <keeganwunder0@gmail.com>
2024-07-05 15:31:33 +0300

    realization of RoleService

--- src/main/java/com/softserve/itacademy/service/RoleService.java
+++ src/main/java/com/softserve/itacademy/service/RoleService.java
@@ -1,13 +1,15 @@
 package com.softserve.itacademy.service;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.Role;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 public interface RoleService {
-    Role create(Role role);
-    Role readById(long id);
-    Role update(Role role);
-    void delete(long id);
+    Role create(Role role) throws NullEntityReferenceException;
+    Role readById(long id) throws EntityNotFoundException;
+    Role update(Role role) throws NullEntityReferenceException, EntityNotFoundException;
+    void delete(long id) throws EntityNotFoundException;
     List<Role> getAll();
 }
--- src/main/java/com/softserve/itacademy/service/impl/RoleServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/RoleServiceImpl.java
@@ -1,40 +1,53 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.Role;
 import com.softserve.itacademy.repository.RoleRepository;
 import com.softserve.itacademy.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
 
+    private static final String ROLE_CANNOT_BE_NULL = "Role cannot be null";
+    private static final String ROLE_NOT_FOUND = "Role not found";
+    private static final String ROLE_NOT_FOUND_WITH_ID = "Role not found with id ";
+
     private final RoleRepository roleRepository;
 
     @Override
-    public Role create(Role role) {
-            return roleRepository.save(role);
+    public Role create(Role role) throws NullEntityReferenceException {
+        if (role == null){
+            throw new NullEntityReferenceException(ROLE_CANNOT_BE_NULL);
+        }
+        return roleRepository.save(role);
     }
 
     @Override
-    public Role readById(long id) {
-        Optional<Role> optional = roleRepository.findById(id);
-            return optional.get();
+    public Role readById(long id) throws EntityNotFoundException {
+       return roleRepository.findById(id)
+               .orElseThrow(() -> new EntityNotFoundException(ROLE_NOT_FOUND));
     }
 
     @Override
-    public Role update(Role role) {
-            Role oldRole = readById(role.getId());
-                return roleRepository.save(role);
+    public Role update(Role role) throws EntityNotFoundException, NullEntityReferenceException {
+        if (role == null){
+            throw new NullEntityReferenceException(ROLE_CANNOT_BE_NULL);
+        }
+        if (!roleRepository.existsById(role.getId())){
+            throw new EntityNotFoundException(ROLE_NOT_FOUND_WITH_ID + role.getId());
+        }
+        return roleRepository.save(role);
     }
 
     @Override
-    public void delete(long id) {
+    public void delete(long id) throws EntityNotFoundException {
         Role role = readById(id);
             roleRepository.delete(role);
     }
