e3b1c2577d4d6ce1e56c0dd976eafbd9a7ac508d
BogdanPhoenix <bohdan.musco@gmail.com>
2024-07-05 11:41:32 +0300

    Adding exceptions to the StateService.

--- src/main/java/com/softserve/itacademy/service/StateService.java
+++ src/main/java/com/softserve/itacademy/service/StateService.java
@@ -1,15 +1,19 @@
 package com.softserve.itacademy.service;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.State;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 public interface StateService {
-    State create(State state);
-    State readById(long id);
-    State update(State state);
-    void delete(long id);
 
-    State getByName(String name);
+    State create(State state) throws NullEntityReferenceException;
+    State readById(long id) throws EntityNotFoundException;
+    State update(State state) throws NullEntityReferenceException, EntityNotFoundException;
+    void delete(long id) throws EntityNotFoundException;
+
+    State getByName(String name) throws EntityNotFoundException;
     List<State> getAll();
+
 }
