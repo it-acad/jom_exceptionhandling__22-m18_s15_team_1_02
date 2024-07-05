27f74c7cf7fdeb677a61d486da520c4e36c7c8e7
Dneprik <sdneprik@gmail.com>
2024-07-06 01:14:58 +0200

    500 + ToDoService

--- src/main/java/com/softserve/itacademy/service/ToDoService.java
+++ src/main/java/com/softserve/itacademy/service/ToDoService.java
@@ -1,14 +1,17 @@
 package com.softserve.itacademy.service;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.ToDo;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.constraints.Null;
 import java.util.List;
 
 public interface ToDoService {
-    ToDo create(ToDo todo);
-    ToDo readById(long id);
-    ToDo update(ToDo todo);
-    void delete(long id);
+    ToDo create(ToDo todo) throws NullEntityReferenceException;
+    ToDo readById(long id) throws EntityNotFoundException;
+    ToDo update(ToDo todo) throws NullEntityReferenceException, EntityNotFoundException;
+    void delete(long id) throws EntityNotFoundException;
 
     List<ToDo> getAll();
     List<ToDo> getByUserId(long userId);
--- src/main/java/com/softserve/itacademy/service/impl/ToDoServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/ToDoServiceImpl.java
@@ -1,10 +1,13 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.ToDo;
 import com.softserve.itacademy.repository.ToDoRepository;
 import com.softserve.itacademy.service.ToDoService;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,19 +23,27 @@ public class ToDoServiceImpl implements ToDoService {
 
     @Override
     public ToDo create(ToDo todo) {
+        if (todo == null) {
+            throw new NullEntityReferenceException("ToDo cannot be null");
+        }
             return todoRepository.save(todo);
     }
 
     @Override
     public ToDo readById(long id) {
         Optional<ToDo> optional = todoRepository.findById(id);
-            return optional.get();
+            return optional.orElseThrow(() -> new EntityNotFoundException("ToDO not found with id " + id));
     }
 
     @Override
     public ToDo update(ToDo todo) {
-            ToDo oldTodo = readById(todo.getId());
-                return todoRepository.save(todo);
+        if (todo == null) {
+            throw new NullEntityReferenceException("ToDo cannot be null");
+        }
+        if (!todoRepository.existsById(todo.getId())) {
+            throw new EntityNotFoundException("ToDo not found with id " + todo.getId());
+        }
+        return todoRepository.save(todo);
     }
 
     @Override
